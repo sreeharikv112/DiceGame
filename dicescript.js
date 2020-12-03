4c72af1bb499a094cc1f7181ffa4e5164b01a341
hari <sreeharikv112@gmail.com>
2020-12-03 18:23:33 +0530

    update with jq

--- dicescript.js
+++ dicescript.js
@@ -1,25 +1,24 @@
-function rollButtonClick() {
+  function rollButtonClick() {
 
-  //create two randoms for 2 dices
-  var random1 = Math.floor(Math.random() * 6) + 1;
+    var random1 = Math.floor(Math.random() * 6) + 1;
 
-  var random2 = Math.floor(Math.random() * 6) + 1;
+    var random2 = Math.floor(Math.random() * 6) + 1;
 
-  //access the image elements
-  var imgFirstDice = document.getElementById("img1");
-  imgFirstDice.setAttribute('src', "images/dice" + random1 + ".png");
+    //access the image elements
+    var imgFirstDice = $("#img1");
+    imgFirstDice.attr('src', "images/dice" + random1 + ".png");
 
-  var imgSecondDice = document.getElementById("img2");
-  imgSecondDice.setAttribute('src', "images/dice" + random2 + ".png");
+    var imgSecondDice = $("#img2");
+    imgSecondDice.attr('src', "images/dice" + random2 + ".png");
 
-  //set default text and elements post rolling dice.
-  document.querySelector("h1").textContent = "Player 2 Wins🏆"
+    //set default text and elements post rolling dice.
+    $("h1").text("Player 2 Wins🏆");
 
-  //modify the elements as per criteria
-  if (random1 > random2) {
-    document.querySelector("h1").textContent = "🏆Player 1 Wins "
-  } else if (random1 == random2) {
-    document.querySelector("h1").textContent = "Its a draw🎭";
+    //modify the elements as per criteria
+    if (random1 > random2) {
+      $("h1").text("🏆Player 1 Wins ");
+    } else if (random1 == random2) {
+      $("h1").text("Its a draw");
+    }
+
   }
-
-}
